a307f59f5ed2c6371a5864bb3f754267b60c0682
= <=>
2023-07-25 07:01:56 +0000

    implementation of the caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+In this module, we create a class BasicCache that inherits from BaseCaching
+and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    This class inherits the BaseCaching class
+    for the module base_caching.py
+    """
+
+    def put(self, key, item):
+        """
+        This function checks if the key or item is equal to None
+        and then assigns them respectively if not
+        """
+
+        if key != None or item != None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        This function returns a specific key
+        """
+
+        return self.cache_data.get(key)
+
+
+
+
+
