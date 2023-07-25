a5e2cfc4feb7e4998882a423ad3217a7e7e00dc8
= <=>
2023-07-25 07:54:59 +0000

    inclusion of the stylings and comments

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+"""
+In this module, we create a class BasicCache that inherits from BaseCaching
+and is a caching system
+"""
 
 
 from base_caching import BaseCaching
@@ -16,8 +19,16 @@ class FIFOCache(BaseCaching):
             self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # Conversion of the dictionary into a list
+            # This assists in the popping of the first index
+            # This is because a dictionary has no indexes but key,value pairs
             dict_list = list(self.cache_data.items())
+
+            # Pop of the first item the list, and extraction of the key,value
+
             removed_key, removed_value = dict_list.pop(0)
+
+            # Removal of the same item using the key, from the dictionary
             self.cache_data.pop(removed_key)
             print(f"DISCARD: {removed_key}")
 
@@ -25,7 +36,7 @@ class FIFOCache(BaseCaching):
         """
         This function returns a specific key
         """
-        
+
         if key not in self.cache_data.keys:
             return None
 
